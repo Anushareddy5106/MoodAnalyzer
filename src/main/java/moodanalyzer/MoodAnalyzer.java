3ddf91495217203e0d4c02b47b63321b49a920f6
Anushareddy5106 <anushachandupatla.2003@gmail.com>
2024-01-03 19:50:33 +0530

    UC1

---
+++ src/main/java/moodanalyzer/MoodAnalyzer.java
@@ -0,0 +1,32 @@
+package moodanalyzer;
+
+//UC1
+public class MoodAnalyzer {
+    static final String SAD_RESULT = "SAD";
+    static final String HAPPY_RESULT = "HAPPY";
+
+    public String message;
+
+    public MoodAnalyzer() {
+        this.message = "";
+    }
+
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    public String analyzeMood() {
+        message = message.toLowerCase();
+
+        String result = "";
+
+        if (message.contains("sad")) {
+            result = SAD_RESULT;
+        } else {
+            result = HAPPY_RESULT;
+        }
+
+        return result;
+    }
+
+}
---
+++ src/test/java/TestMoodAnalyzer.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import moodanalyzer.MoodAnalyzer;
+
+public class TestMoodAnalyzer {
+    public MoodAnalyzer moodAnalyzer;
+
+    @Test
+    public void testAnalazyMoodSad() {
+        moodAnalyzer = new MoodAnalyzer("I am in Sad mood");
+        String result = moodAnalyzer.analyzeMood();
+
+        assertEquals("SAD", result);
+    }
+
+    @Test
+    public void testAnalazyMoodHappy() {
+        moodAnalyzer = new MoodAnalyzer("I am in Any mood");
+        String result = moodAnalyzer.analyzeMood();
+
+        assertEquals("HAPPY", result);
+    }
+
+}
